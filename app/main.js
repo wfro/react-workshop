621a64b32a8d6b5e61fc0d2a5dedabb5ef24224f
Will Faurot <will@influxdb.com>
2015-07-26 18:37:53 -0700

    Clean up main.js

--- app/main.js
+++ app/main.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import Ideabox from './components/Ideabox';
 
-// Static ideas until we can implement ajax stuff
-var IDEAS = [
-  {id: 1, title: 'Lorem', body: 'ipsum'},
-  {id: 2, title: 'Jon', body: 'Snow'},
-  {id: 3, title: 'Eddard', body: 'Stark'}
-];
-
-React.render(<Ideabox data={IDEAS} />, document.getElementById('root'));
+React.render(<Ideabox url='http://localhost:3000/ideas' />, document.getElementById('root'));
